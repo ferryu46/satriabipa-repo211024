0a893da554b119ebe9f14bf7fc2bb61e0b53ba6b
ferryu46 <ferry.yunkurniawan@dikbud.belajar.id>
2024-10-17 15:32:55 +0700

    first revised commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/opening', (req, res) => {
+    res.render('opening');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/chat', (req, res) => {
+    res.render('chatAI');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
